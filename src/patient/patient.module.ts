4ef5c52ece142c18ef153717375952978720b9fa
PRINCE-MEHTA91 <princekumar01zc@gmail.com>
2026-06-25 13:42:18 +0530

    fix it

--- src/patient/patient.module.ts
+++ src/patient/patient.module.ts
@@ -1,26 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PatientProfile } from './entities/patient-profile.entity';
-import { Appointment } from '../appointment/entities/appointment.entity';
-import { DoctorProfile } from '../doctor/entities/doctor-profile.entity';
-import { RecurringAvailability } from '../doctor/entities/recurring-availability.entity';
-import { CustomAvailability } from '../doctor/entities/custom-availability.entity';
 import { PatientService } from './patient.service';
 import { PatientController } from './patient.controller';
-import { AppointmentService } from './appointment.service';
-import { AppointmentController } from './appointment.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       PatientProfile,
-      Appointment,
-      DoctorProfile,
-      RecurringAvailability,
-      CustomAvailability,
     ]),
   ],
-  controllers: [PatientController, AppointmentController],
-  providers: [PatientService, AppointmentService],
+  controllers: [PatientController],
+  providers: [PatientService],
 })
 export class PatientModule {}
